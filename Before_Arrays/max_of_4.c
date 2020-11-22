f07d6b242e0ab6a14fe08488a0025201d9be4d9e
eyidis04 <66193516+eyidis04@users.noreply.github.com>
2020-11-22 18:17:10 +0300

    Update max_of_4.c

--- Before_Arrays/max_of_4.c
+++ Before_Arrays/max_of_4.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int number1, number2, number3, number4;
+
+int max4(int a, int b, int c, int d)
+{
+   return  a > b && a > c &&  a > d ? a :  b > a && b > c && b > d? b : c > a && c > b && c > d ? c : d ;
+}
+
+int main()
+{
+    while (1)   {
+      
+    printf("Enter four number: \n");
+    scanf("%d%d%d%d", &number1, &number2, &number3, &number4);
+    printf("max of four: %d \n", max4 (number1, number2, number3, number4));
+    }
+}
 
