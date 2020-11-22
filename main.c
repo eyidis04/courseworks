672ec015593cfbfc78087febeb36a5a3d3a49caa
eyidis04 <66193516+eyidis04@users.noreply.github.com>
2020-11-22 15:56:21 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float number1, number2, number3;
+float max;
+
+float max3 (float number1, float number2, float number3)
+{
+    /*if( b >a && b > c)
+        max = b;
+
+    else if (c > a && c > b)
+        max = c;
+
+    else
+        max = a;*/
+
+    max = number1 > number2 ? (number1 > number3 ? number1 : number3) : number2 < number3 ? number3 : number2 ;
+    return max;
+
+}
+int main()
+{
+    while (1)
+    {
+    printf("Enter three numbers :");
+    scanf("%f%f%f" , &number1, &number2, &number3 );
+    printf("Biggest number of the entered values: %f \n",max3(number1, number2, number3));
+
+    getch();
+    }
+}
